e20efac665e9b462fd988c6624463446812f8f03
Ashikssm143 <mdrokonuzzamanashik@gmail.com>
2024-09-15 16:43:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,117 @@
+//Nested if
+//Materimony site
+
+
+let gender = "Male"
+let age = 35
+let area = "Dhaka"
+let religion = "Muslim"
+let professions = "Engineering"
+
+if (gender == "Male") {
+   if (age >= 25 &&  age < 40) {
+    if (area == "Dhaka") {
+        if (religion == "Muslim") {
+            if (professions =="Engineering") {
+              console.log("You are perfect candidate");
+              
+            }
+            else{
+                console.log("Professions not match");
+                
+            }
+          
+      }
+      else{
+        console.log("Religion Not Match");
+    }
+        
+    }
+    else{
+        console.log("Area Not Match");
+    }
+    
+        
+    }
+    else{
+        console.log("Age Not Match");
+    }
+   }
+   
+else{
+    console.log("You are not perfect");
+    
+}
+
+
+// For 
+for (let i=90; i<=100; i++) {
+    console.log(i);
+    
+}
+
+
+
+
+//Namota Number
+let n = "NamotaNumber"
+console.log(n);
+ 
+let namotaNumber = 67 
+
+for (let i=10; i<=20; i++) {
+    console.log(namotaNumber + " X " + i + " = " + namotaNumber*i);
+    
+}
+
+
+//Odd Number
+let o = "OddNumber"
+console.log(o);
+
+for (let i = 1; i<=30; i+=2) {
+   
+    console.log(i);
+    
+    
+}
+
+        
+//Even Number
+let e = "EvenNumber"
+console.log(e);
+for (let i = 2; i<=30; i+=2) {
+   
+    console.log(i);
+    
+    
+}
+
+
+//Even Number
+let oe = "OddEvenNumber"
+console.log(oe);
+for (let i = 2; i<=30; i+=3) {
+   
+    console.log(i);
+    
+    
+}
+
+
+//Cell Number
+let cell = "CellNumber"
+console.log(cell);
+
+let x = "+880"
+let y = 1384948599
+console.log(x+y);
+
+        
+//Array 
+let array = "ArrayNumber"
+console.log(array);
+
+let teams = ["Ashik", "Akram", "Jibon", "Mahir", "Evan"]
+  console.log(teams);
+  
